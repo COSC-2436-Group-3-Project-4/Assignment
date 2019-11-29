c334843dcee9c4ec283326ba5304b79c7b9f44ae
FoolishMagician <56321818+FoolishMagician@users.noreply.github.com>
2019-11-29 14:02:15 -0600

    Update Temp-Solution.py

--- Temp-Solution.py
+++ Temp-Solution.py
@@ -1,18 +1,19 @@
-#from PreProj import Building
-#building=Building()
-
+# Doubly Linked List Queue
+''' In the Queue
+        Adjust the tail when adding item
+        Adjust the head when removing item
+'''
+#n1 = bldg.head #lowest floor
+#n10 = bldg.tail #highest floor
 from node import DNode
 
-class Building:
-	number_of_floors = 0                                                                                                                # sets number_of_floors variable to 0
-    customer_list = []                                                                                                                  # creates an empty array for customer_list
-    elevator = 0                                                                                                                        # sets elevator variable to 0
+class Queue:
     def __init__(self):
         self.head = None
         self.tail = None
-    # This method adds floors to the queue
-    def floors(self,item):
-        if self.head == None: #If the bldg is empty
+    # This method adds item to the queue
+    def add(self,item):
+        if self.head == None: #If the queue is empty
             self.head = item
             self.tail = item
         else:
@@ -21,8 +22,23 @@ class Building:
             item.previous = self.tail
             # change the tail to point to this item
             self.tail = item
+
+    def append(self, data):
+        if self.head is None:
+            new_node = DNode(data)
+            new_node.prev = None
+            self.head = new_node
+        else:
+            new_node = DNode(data)
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+            new_node.prev = cur
+            new_node.next = None
+
     # This method removes the item to the queue
-    def remove(self):
+    def remove(self,item):
         item = self.head # temporarily save the head to item
         print('Removing ...',item.data)
         # adjust the head to point to the next item in Queue
@@ -60,7 +76,8 @@ class Building:
         # Special case for the empty linked list  
         if self.head is None: 
             new_node.next = self.head 
-            self.head = new_node 
+            self.head = new_node
+
   
         # Special case for head at end 
         elif int(self.head.data) >= int(new_node.data): 
@@ -78,14 +95,107 @@ class Building:
             new_node.next = current.next
             current.next = new_node 
         
-    def delete(self,item):
-        item = self.getItem(DNode(item))
-        nodep = item.previous
-        noden = item.next
-        nodep.next = noden
-        noden.previous = nodep
-        return item
+##    def delete(self,item):
+##        item = self.getItem(DNode(item))
+##        nodep = item.previous
+##        noden = item.next
+##        nodep.next = noden
+##        noden.previous = nodep
+##        return item
+    
+    def delete(self, key):
+        cur = self.head
+        while cur:
+            if cur.data == key and cur == self.head:
+                # Case 1:
+                if not cur.next:
+                    cur = None 
+                    self.head = None
+                    return
 
+                # Case 2:
+                else:
+                    nxt = cur.next
+                    cur.next = None 
+                    nxt.previous = None
+                    cur = None
+                    self.head = nxt
+                    return 
+
+            elif cur.data == key:
+                # Case 3:
+                if cur.next:
+                    nxt = cur.next 
+                    previous = cur.previous
+                    previous.next = nxt
+                    nxt.previous = previous
+                    cur.next = None 
+                    cur.previous = None
+                    cur = None
+                    return
+
+                # Case 4:
+                else:
+                    previous = cur.previous 
+                    previous.next = None 
+                    cur.previous = None 
+                    cur = None 
+                    return 
+            cur = cur.next
+
+    def delete_node(self, node):
+        cur = self.head
+        while cur:
+            if cur == node and cur == self.head:
+                # Case 1:
+                if not cur.next:
+                    cur = None 
+                    self.head = None
+                    return
+
+                # Case 2:
+                else:
+                    nxt = cur.next
+                    cur.next = None 
+                    nxt.previous = None
+                    cur = None
+                    self.head = nxt
+                    return 
+
+            elif cur == node:
+                # Case 3:
+                if cur.next:
+                    nxt = cur.next 
+                    previous = cur.previous
+                    previous.next = nxt
+                    nxt.previous = previous
+                    cur.next = None 
+                    cur.previous = None
+                    cur = None
+                    return
+
+                # Case 4:
+                else:
+                    previous = cur.previous 
+                    previous.next = None 
+                    cur.previous = None 
+                    cur = None 
+                    return 
+            cur = cur.next
+            
+    def remove_duplicates(self):
+        cur = self.head 
+        seen = dict()
+        while cur:
+            if cur.data not in seen:
+                seen[cur.data] = 1
+                cur = cur.next
+            else:
+                nxt = cur.next
+                self.delete_node(cur)
+                cur = nxt
+
+                
     def getItem(self, item):
         node = self.head
         while node.next != None:
@@ -105,8 +215,17 @@ class Building:
             print (temp.data) 
             temp = temp.next
 
+class Building():
+    def __init__(self,floors):
+        self.__floors = floors
+    def getFloors(self):
+        return self.__floors
+
+
+
 class Elevator():
     def __init__(self):
+        self.currentfloor=0
         self.elevatorstatus= bldg.head.data
     def set_elevatorStatus(self,s):
         self.elevatorstatus=s
@@ -137,124 +256,37 @@ class Request():
         return self.requestedfloor
 
 
-
-bldg = Building()
-
-bldg.floors(DNode(1))
-bldg.floors(DNode(2))
-bldg.floors(DNode(3))
-bldg.floors(DNode(4))
-bldg.floors(DNode(5))
-bldg.floors(DNode(6))
-bldg.floors(DNode(7))
-bldg.floors(DNode(8))
-bldg.floors(DNode(9))
-bldg.floors(DNode(10))
-
-
-n1 = bldg.head #lowest floor
-n2 = n1.next #next item
-n3 = n2.next #next item
-n4 = n3.next #next item
-n5 = n4.next #next item
-n6 = n5.next #next item
-n7 = n6.next #next item
-n8 = n7.next #next item
-n9 = n8.next #next item
-n10 = bldg.tail #highest floor
-
-print(bldg.tail.data)
-
-print(n1.data)
-print(n2.data)
-print(n3.data)
-
-el=Elevator()
-#el.set_currentFloor(5)
-print("Elevator Status",el.get_elevatorStatus())
-
-req=Request()
-req.set_currentFloor(bldg.tail.data)
-req.set_Direction(0)
-req.set_RequestedFloor(3)
-
-print("Current Floor",req.get_currentFloor())
-print("Requested Direction",req.get_Direction())
-print("Requested Floor", req.get_RequestedFloor())
-
-
 queue = Queue()
-
-#Push n number of items in the stack
-
-queue.add(DNode('1'))
-queue.add(DNode('3'))
-queue.add(DNode('5'))
-queue.add(DNode('7'))
-queue.add(DNode('8'))
-queue.add(DNode('9'))
+#bldg = Building()
 
 
+floors=int(input("How many floors does this bulding have?"))
+#Sets floor 1 as lobby, floor will be range
+total_floors=Building(floors)
+new_node = DNode(int(1))
+queue.sortedInsert(new_node)#This will be the lobby
+flag = 'y'
+while flag == 'y':
+     
 
-#marshalling through the queue
-n1 = queue.head #head item
-n2 = n1.next #next item
-n3 = n2.next #next item
-n4 = n3.next #next item
-n5 = n4.next #next item
-n6 = n5.next #next item
+    floorrequest=int(input("What floor do you want to go to?"))
+    #Conditional Statement to remove redundancies
+    #   if floorrequest == data
+    #Conditonal Statement for request to not be higher than number than floors
+    #   if floorrequest >> floor... return try again.
+    new_node = DNode(int(floorrequest))
+    queue.append(int(floorrequest))
+    queue.sortedInsert((new_node))
+    #queue.remove_duplicates()
+    flag = input("Do you want to add a floor request :")
 
-
-print("Traversing the queue")
-print(n1.data)
-print(n2.data)
-print(n3.data)
-print("Insert Sort")
-queue.sortedInsert(DNode('9'))
-
-print(n1.data)
-print(n2.data)
-print(n3.data)
-print(n4.data)
-print(n5.data)
-print(n6.data)
-
-print("Queue Count")
-print(queue.getCount())
-
-
-#insert Doug
-queue.insert(DNode('4'),'5')
-#Deleting Doug
-queue.delete('4')
-
-# marshalling backwards through double link
-print("Traversing reverse the queue")
-print(n3.data)
-print(n3.previous.data)
-print(n2.previous.data)
-# removing 
-n1 = queue.remove()
-print(n1.data)
-n2 = queue.remove()
-print(n2.data)
-n3 = queue.remove()
-print(n3.data)
-print("Sorted Insert")
-# Driver program 
-new_node = DNode(5) 
-queue.sortedInsert(new_node) 
-new_node = DNode(10) 
-queue.sortedInsert(new_node) 
-new_node = DNode(7) 
-queue.sortedInsert(new_node) 
-new_node = DNode(3) 
-queue.sortedInsert(new_node) 
-new_node = DNode(1) 
-queue.sortedInsert(new_node) 
-new_node = DNode(9) 
-queue.sortedInsert(new_node) 
 print ("Create Linked List")
-queue.printList()             
-        
-        
+queue.printList()
+queue.remove_duplicates()
+print ("Create Linked List After Removing Duplicates")
+queue.printList()
+print("Sort Request and add top floor")
+queue.sortedInsert(int(floors))
+queue.printList()
+marshall=queue.getCount()
+print("Number of Nodes", marshall)
